7ddc95f6036b9136c1bdfd813d470a2f874ef6ff
ynnhi2607 <huynhyennhi2607@gmail.com>
2026-09-20 11:04:33 +0700

    initial backend

---
+++ src/main/java/com/social/be/HelloController.java
@@ -0,0 +1,13 @@
+package com.social.be;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/api/hello")
+    public String hello() {
+        return "Hello ConnectSpace!";
+    }
+}
---
+++ src/test/java/com/social/be/BeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.social.be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
